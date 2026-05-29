29687bb586f0f637f4b85130835a9fa0758613bb
Janne <schoen.janne@gmail.com>
2026-05-29 20:21:43 +0200

    variable renaming, formatting comments

--- main.c
+++ main.c
@@ -19,208 +19,227 @@ int main(int argc, char * argv[]){
         return 1;
     }
 
-    char * inputFile = argv[1];
-    char * responseFile = argv[2];
+    char * input_file = argv[1];
+    char * response_file = argv[2];
     unsigned degree = (unsigned)atoi(argv[4]);
-    unsigned matDim = degree + 1;
+    unsigned matrix_dimensions = degree + 1;
 
-    // Counting lines of file
-    unsigned countLines(char * fileName){
-        unsigned numLines = 0;
-        double num;
-        FILE * file = fopen(fileName, "r");
-        while(fscanf(file, "%lf", &num) != EOF){
-            numLines ++;
+
+    // Count lines of file
+
+    unsigned count_lines(char * file_name){
+        unsigned number_of_lines = 0;
+        double number;
+        FILE * file = fopen(file_name, "r");
+        while(fscanf(file, "%lf", &number) != EOF){
+            number_of_lines ++;
         }
-        return numLines;
+        return number_of_lines;
     }
 
-    // Reading double numbers of file into array
-    void readNumbers(char * fileName, double * array, unsigned num){
-        FILE * file = fopen(fileName, "r");
-        for(unsigned i = 0; i < num; i++){
+    // Read double numbers of file into array
+
+    void read_numbers(char * file_name, double * array, unsigned number){
+        FILE * file = fopen(file_name, "r");
+        for(unsigned i = 0; i < number; i++){
             fscanf(file, "%lf", &array[i]);
         }
     }
 
-    unsigned numInputs = countLines(inputFile);
-    unsigned numPoints = countLines(responseFile);
+    unsigned number_of_inputs = count_lines(input_file);
+    unsigned number_of_points = count_lines(response_file);
 
-    double inputs[numInputs];
-    double responses[numPoints];
-    readNumbers(inputFile, inputs, numInputs);
-    readNumbers(responseFile, responses, numPoints);
+    double inputs[number_of_inputs];
+    double responses[number_of_points];
+    read_numbers(input_file, inputs, number_of_inputs);
+    read_numbers(response_file, responses, number_of_points);
 
-    printf("Number of observations: %u\n", numPoints);
-    printf("Inputs with unknown response: %u\n", numInputs - numPoints);
+    printf("Number of observations: %u\n", number_of_points);
+    printf("Inputs with unknown response: %u\n", number_of_inputs - number_of_points);
 
 
-    // Creating design / feature matrix
+    // Create design / feature matrix
 
-    double designMat[numPoints][matDim];
-    for(unsigned i = 0; i < numPoints; i++){
-        for(unsigned j = 0; j < matDim; j++){
-            designMat[i][j] = pow(inputs[i], j);
+    double design_matrix[number_of_points][matrix_dimensions];
+    for(unsigned i = 0; i < number_of_points; i++){
+        for(unsigned j = 0; j < matrix_dimensions; j++){
+            design_matrix[i][j] = pow(inputs[i], j);
         }
     }
 
-    // Transposing design matrix
+    // Transpose design matrix
 
-    double transpose[matDim][numPoints];
-    for(unsigned i = 0; i < matDim; i++){
-        for(unsigned j = 0; j < numPoints; j++){
-            transpose[i][j] = designMat[j][i];
+    double transpose[matrix_dimensions][number_of_points];
+    for(unsigned i = 0; i < matrix_dimensions; i++){
+        for(unsigned j = 0; j < number_of_points; j++){
+            transpose[i][j] = design_matrix[j][i];
         }
     }
 
-    // Calculating Gram matrix (X^T * X)
+    // Calculate Gram matrix (X^T * X)
     
-    double gramMat[matDim][matDim];
-    for(unsigned i = 0; i < matDim; i++){
-        for(unsigned j = 0; j < matDim; j++){
+    double gram_matrix[matrix_dimensions][matrix_dimensions];
+    for(unsigned i = 0; i < matrix_dimensions; i++){
+        for(unsigned j = 0; j < matrix_dimensions; j++){
             double value = 0;
-            for(unsigned k = 0; k < numPoints; k++){
-                value += transpose[i][k] * designMat[k][j];
+            for(unsigned k = 0; k < number_of_points; k++){
+                value += transpose[i][k] * design_matrix[k][j];
             }
-            gramMat[i][j] = value;
+            gram_matrix[i][j] = value;
         }
     }
 
-    // Inverting the Gram matrix using Gaussian Elimination
 
-    // Setting up augmented matrix (covariance matrix + identity matrix)
-    double augMat[matDim][matDim * 2];
-    for(unsigned i = 0; i < matDim; i++){
-        for(unsigned j = 0; j < matDim * 2; j++){
-            if(j < matDim){
-                augMat[i][j] = gramMat[i][j];
-            } else if(i == j - matDim){
-                augMat[i][j] = 1;
+    // Invert the Gram matrix using Gaussian Elimination
+
+    // Set up augmented matrix (covariance matrix + identity matrix)
+
+    double augmented_matrix[matrix_dimensions][matrix_dimensions * 2];
+    for(unsigned i = 0; i < matrix_dimensions; i++){
+        for(unsigned j = 0; j < matrix_dimensions * 2; j++){
+            if(j < matrix_dimensions){
+                augmented_matrix[i][j] = gram_matrix[i][j];
+            } else if(i == j - matrix_dimensions){
+                augmented_matrix[i][j] = 1;
             } else{
-                augMat[i][j] = 0;
+                augmented_matrix[i][j] = 0;
             }
         }
     }
 
-    // Dividing a line by its part of the diagonale
-    void normLine(unsigned i){
-        double divisor = augMat[i][i];
-        for(unsigned j = 0; j < matDim * 2; j++){
-            augMat[i][j] /= divisor;
+
+    // Divide a line by its part of the diagonale
+
+    void norm_line(unsigned i){
+        double divisor = augmented_matrix[i][i];
+        for(unsigned j = 0; j < matrix_dimensions * 2; j++){
+            augmented_matrix[i][j] /= divisor;
         }
     }
 
-    // Subtrating line y times z from line x
-    void linCombSub(unsigned x, unsigned y, double z){
-        for(unsigned i = 0; i < matDim * 2; i++){
-            augMat[x][i] -= augMat[y][i] * z;
+
+    // Subtract line y times z from line x
+
+    void subtract_linear_combination(unsigned x, unsigned y, double z){
+        for(unsigned i = 0; i < matrix_dimensions * 2; i++){
+            augmented_matrix[x][i] -= augmented_matrix[y][i] * z;
         }
     }
 
-    void switchLines(unsigned x, unsigned y){
-        for(unsigned i = 0; i < matDim * 2; i++){
-            double value = augMat[x][i];
-            augMat[x][i] = augMat[y][i];
-            augMat[y][i] = value;
+    void switch_lines(unsigned x, unsigned y){
+        for(unsigned i = 0; i < matrix_dimensions * 2; i++){
+            double value = augmented_matrix[x][i];
+            augmented_matrix[x][i] = augmented_matrix[y][i];
+            augmented_matrix[y][i] = value;
         }
     }
 
-    for(unsigned i = 0; i < matDim; i++){
-        unsigned timesSwitched = 0;
-        while(augMat[i][i] == 0){
-            if(timesSwitched == matDim || i+1 == matDim){
+    for(unsigned i = 0; i < matrix_dimensions; i++){
+        unsigned times_switched = 0;
+        while(augmented_matrix[i][i] == 0){
+            if(times_switched == matrix_dimensions || i+1 == matrix_dimensions){
                 printf("Error: matrix not invertible.\n");
                 return 1;
             }
+
             // if value at [i][i] is 0, switch lines
             // -> makes sure diagonale only has non-zero values
-            switchLines(i, i+1);
-            timesSwitched++;
-        }
-        normLine(i);
 
-        for(unsigned j = 0; j < matDim; j++){
+            switch_lines(i, i+1);
+            times_switched++;
+        }
+        norm_line(i);
+
+        for(unsigned j = 0; j < matrix_dimensions; j++){
             if(j != i){
-                double factor = augMat[j][i];
-                linCombSub(j, i, factor);
+                double factor = augmented_matrix[j][i];
+                subtract_linear_combination(j, i, factor);
             }
         }
     }
 
-    for(unsigned i = 0; i < matDim; i++){
-        for(unsigned j = 0; j < matDim; j++){
-            if((i == j && augMat[i][j] != 1) || (i != j && augMat[i][j] != 0)){
+    for(unsigned i = 0; i < matrix_dimensions; i++){
+        for(unsigned j = 0; j < matrix_dimensions; j++){
+            if((i == j && augmented_matrix[i][j] != 1) || (i != j && augmented_matrix[i][j] != 0)){
                 printf("Error: matrix could not be inverted.\n");
                 return 1;
             }
         }
     }
 
-    // Reading inverse from modified augmented matrix
 
-    double inverse[matDim][matDim];
-    for(unsigned i = 0; i < matDim; i++){
-        for(unsigned j = 0; j < matDim; j++){
-            inverse[i][j] = augMat[i][j+matDim];
+    // Read inverse from modified augmented matrix
+
+    double inverse[matrix_dimensions][matrix_dimensions];
+    for(unsigned i = 0; i < matrix_dimensions; i++){
+        for(unsigned j = 0; j < matrix_dimensions; j++){
+            inverse[i][j] = augmented_matrix[i][j+matrix_dimensions];
         }
     }
 
-    // Calculating cross product vector (transpose of design matrix * response vector)
-    double crossProductVec[matDim];
-    for(unsigned i = 0; i < matDim; i++){
+
+    // Calculate cross product vector (transpose of design matrix * response vector)
+
+    double cross_product_vector[matrix_dimensions];
+    for(unsigned i = 0; i < matrix_dimensions; i++){
         double value = 0;
-        for(unsigned j = 0; j < numPoints; j++){
+        for(unsigned j = 0; j < number_of_points; j++){
             value += transpose[i][j] * responses[j];
         }
-        crossProductVec[i] = value;
+        cross_product_vector[i] = value;
     }
 
-    // Calculating coefficients (inverse of Gram matrix * cross product vector)
-    double coefficients[matDim];
-    for(unsigned i = 0; i < matDim; i++){
+
+    // Calculate coefficients (inverse of Gram matrix * cross product vector)
+
+    double coefficients[matrix_dimensions];
+    for(unsigned i = 0; i < matrix_dimensions; i++){
         double value = 0;
-        for(unsigned j = 0; j < matDim; j++){
-            value += inverse[i][j] * crossProductVec[j];
+        for(unsigned j = 0; j < matrix_dimensions; j++){
+            value += inverse[i][j] * cross_product_vector[j];
         }
         coefficients[i] = value;
     }
 
+
     // Predict responses to inputs based on calculated function
 
-    double predictions[numInputs];
-    for(unsigned i = 0; i < numInputs; i++){
+    double predictions[number_of_inputs];
+    for(unsigned i = 0; i < number_of_inputs; i++){
         predictions[i] = coefficients[0];
-        for(unsigned j = 1; j < matDim; j++){
+        for(unsigned j = 1; j < matrix_dimensions; j++){
             predictions[i] += coefficients[j] * pow(inputs[i], j);
         }
     }
 
+
     // Calculate mean squared error
 
-    double squaredErrors[numPoints];
-    for(unsigned i = 0; i < numPoints; i++){
-        squaredErrors[i] = pow(predictions[i] - responses[i], 2);
+    double squared_errors[number_of_points];
+    for(unsigned i = 0; i < number_of_points; i++){
+        squared_errors[i] = pow(predictions[i] - responses[i], 2);
     }
-    double meanSquaredError = 0;
-    for(unsigned i = 0; i < numPoints; i++){
-        meanSquaredError += squaredErrors[i];
+    double mean_squared_error = 0;
+    for(unsigned i = 0; i < number_of_points; i++){
+        mean_squared_error += squared_errors[i];
     }
-    meanSquaredError /= numPoints;
+    mean_squared_error /= number_of_points;
 
-    printf("\nMSE: %.5f\n", meanSquaredError);
-    printf("MSE Root: %.5f\n", pow(meanSquaredError, 0.5));
+    printf("\nMSE: %.5f\n", mean_squared_error);
+    printf("MSE Root: %.5f\n", pow(mean_squared_error, 0.5));
 
     printf("\nCoefficients:\n");
     printf("%lf", coefficients[0]);
-    for(unsigned i = 1; i < matDim; i++){
+    for(unsigned i = 1; i < matrix_dimensions; i++){
         printf(" + %lf x^%u", coefficients[i], i);
     }
     printf("\n");
 
 
-    // Writing predictions to file
+    // Write predictions to file
+    
     FILE * file = fopen(argv[3], "w");
-    for(unsigned i = 0; i < numInputs; i++){
+    for(unsigned i = 0; i < number_of_inputs; i++){
         fprintf(file, "%lf\n", predictions[i]);
     }
     fclose(file);
